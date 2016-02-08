7723bad72ae1459143880ac79562e0b1ec526060
Davide Setti <davide.setti@gmail.com>
2016-02-08 17:57:22 +0100

    add support for ini options

--- pytest_blockage.py
+++ pytest_blockage.py
@@ -59,6 +59,8 @@ def pytest_addoption(parser):
     group = parser.getgroup('blockage')
     group.addoption('--blockage', action='store_true',
                     help='Block network requests during test run')
+    parser.addini(
+        'blockage', 'Block network requests during test run', default=False)
     group.addoption(
         '--blockage-http-whitelist',
         action='store',
@@ -66,11 +68,20 @@ def pytest_addoption(parser):
             'hostnames',
         default=''
     )
+    parser.addini(
+        'blockage-http-whitelist',
+        'Do not block HTTP requests to this comma separated list of hostnames',
+        default=''
+    )
+
 
 def pytest_sessionstart(session):
     config = session.config
-    if config.option.blockage:
-        http_whitelist = config.option.blockage_http_whitelist.split(',')
+
+    if config.option.blockage or config.getini('blockage'):
+        http_whitelist_str = config.option.blockage_http_whitelist or config.getini('blockage-http-whitelist')
+        http_whitelist = http_whitelist_str.split(',')
+
         smtp_whitelist = []
 
         block_http(http_whitelist)
