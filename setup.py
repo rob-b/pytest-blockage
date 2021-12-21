23536485a94aacdffe9bbcbf6887e0d38d00252e
Davide Setti <davide.setti@gmail.com>
2021-12-21 08:42:26 +0100

    bump version to 0.2.3 and fix a packaging issue

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 
 setup(
     name='pytest-blockage',
-    version='0.2.2',
+    version='0.2.3',
     description='Disable network requests during a test run.',
     long_description=(open('README.rst').read() +
                       open('CHANGELOG.rst').read()),
+    long_description_content_type='text/x-rst',
     license='BSD',
     install_requires=['pytest'],
     py_modules=['pytest_blockage'],
